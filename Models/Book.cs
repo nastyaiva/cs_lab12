4cb28592e26814db2020b5a9da0278dac47d0a85
nastyaiva <148487824+nastyaiva@users.noreply.github.com>
2024-12-29 16:24:42 +0300

    Create Book.cs

---
+++ Models/Book.cs
@@ -0,0 +1,10 @@
+namespace laboratory_12.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }          // Уникальный идентификатор книги
+        public string Title { get; set; }    // Название книги
+        public string Author { get; set; }   // Автор книги
+       
+    }
+}
