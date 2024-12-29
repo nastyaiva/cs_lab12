db85173521afbfb291cf3a69c4aec59e36d3fdd1
nastyaiva <148487824+nastyaiva@users.noreply.github.com>
2024-12-29 16:25:05 +0300

    Add files via upload

---
+++ Models/Loan.cs
@@ -0,0 +1,11 @@
+﻿namespace laboratory_12.Models
+{
+    public class Loan
+    {
+        public int Id { get; set; }          // Уникальный идентификатор займа
+        public int MemberId { get; set; }    // Идентификатор члена библиотеки, который взял книгу
+        public int BookId { get; set; }      // Идентификатор книги, которая была взята
+        public DateTime LoanDate { get; set; } // Дата займа
+        
+    }
+}
---
+++ Models/Member.cs
@@ -0,0 +1,9 @@
+﻿namespace laboratory_12.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }          // Уникальный идентификатор члена библиотеки
+        public string Name { get; set; }     // Имя члена библиотеки
+        public string Email { get; set; }    // Email члена библиотеки
+    }
+}
